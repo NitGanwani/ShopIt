046ad64d461c2a0cbe9b3e093e1519677a9aa570
Nitin Ganwani <nitganwani@gmail.com>
2023-08-04 13:10:01 +0100

    Add initial styles

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,13 +2,17 @@ import { Navbar, Nav, Container, NavbarBrand } from "react-bootstrap";
 import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <NavbarBrand href="/">ShopIt</NavbarBrand>
+          <NavbarBrand href="/">
+            <img src={logo} alt="shopit logo" />
+            ShopIt
+          </NavbarBrand>
           <NavbarToggle aria-controls="basic-navbar-nav"></NavbarToggle>
           <NavbarCollapse id="basic-navbar-nav">
             <Nav className="ms-auto">
