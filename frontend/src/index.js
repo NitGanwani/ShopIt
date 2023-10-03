a4b04b70bae832e1aa746995cce63297494964cd
Nitin <nitganwani@gmail.com>
2023-10-03 15:58:15 +0200

    Add rating and product detail componentes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,11 +11,13 @@ import {
 import "./assets/styles/index.css";
 import "./assets/styles/bootstrap.custom.css";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen></HomeScreen>} />
+      <Route path="/product/:id" element={<ProductScreen></ProductScreen>} />
     </Route>
   )
 );
