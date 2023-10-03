9cdfc117a0acf76bcc9e710e6806424fe954f1c2
Nitin <nitganwani@gmail.com>
2023-10-03 13:39:03 +0200

    Add react router dom and bootstrap

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,27 +3,34 @@ import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import logo from "../assets/logo.png";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <NavbarBrand href="/">
-            <img src={logo} alt="shopit logo" />
-            ShopIt
-          </NavbarBrand>
+          <LinkContainer to="/">
+            <NavbarBrand>
+              <img src={logo} alt="shopit logo" />
+              ShopIt
+            </NavbarBrand>
+          </LinkContainer>
           <NavbarToggle aria-controls="basic-navbar-nav"></NavbarToggle>
           <NavbarCollapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
-                <FaShoppingCart />
-                Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <FaUser />
-                Sign In
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart />
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser />
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </NavbarCollapse>
         </Container>
