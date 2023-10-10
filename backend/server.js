ba8f8bff1b5da8c4f87ab5a279a1690dfb0a5eaf
Nitin <nitganwani@gmail.com>
2023-10-10 10:59:49 +0200

    Add routes and middleware

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-import express from "express";
-import products from "./data/products.js";
-import dotenv from "dotenv";
-import dbConnect from "./config/db.js";
+import express from 'express';
+import productRoutes from './routes/productRoutes.js';
+import dotenv from 'dotenv';
+import dbConnect from './config/db.js';
 dotenv.config();
 
 dbConnect();
@@ -10,18 +10,11 @@ const port = process.env.PORT || 7000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is ready");
+app.get('/', (req, res) => {
+  res.send('Server is ready');
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((product) => product._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
